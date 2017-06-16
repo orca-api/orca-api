267b1ccdf9386b9c03ede8107d4d52df1a62844c
Kouji Takao <kouji@netlab.jp>
2017-06-16 15:19:48 +0900

    自動生成されたテストを修正
    不要なテストを削除して、英語での説明を省力できるようにdescribeを使ってVERSION定
    数のチェックをするように修正した。

--- spec/orca_api_spec.rb
+++ spec/orca_api_spec.rb
@@ -1,11 +1,9 @@
 require "spec_helper"
 
 RSpec.describe OrcaApi do
-  it "has a version number" do
-    expect(OrcaApi::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'VERSION' do
+    it do
+      expect(OrcaApi::VERSION).not_to be nil
+    end
   end
 end
