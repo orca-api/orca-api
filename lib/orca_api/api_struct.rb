f0ced448aeea8ad0853d35b4431795bc414df172
Kouji Takao <kouji@netlab.jp>
2017-07-04 14:31:46 +0900

    OrcaApi::HealthPublicInsuranceを実装(自動テストは未実装)(#5)

--- lib/orca_api/api_struct.rb
+++ lib/orca_api/api_struct.rb
@@ -3,9 +3,37 @@
 module OrcaApi
   # 日レセAPIで扱う構造体を表現するクラス
   class ApiStruct
-    def self.define_accessors(json_name_mappings, struct_mappings = {})
+    def self.json_name_to_attr_name(name)
+      name
+        .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+        .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+        .downcase
+    end
+
+    def self.define_accessors(mappings)
+      json_name_mappings = {}
+      struct_mappings = {}
+      array_names = []
+
+      mappings.each do |mapping|
+        if mapping.is_a?(String)
+          mapping = [mapping, {}]
+        end
+        if !mapping[1][:name]
+          mapping[1][:name] = json_name_to_attr_name(mapping[0])
+        end
+        json_name_mappings[mapping[0]] = mapping[1][:name]
+        if mapping[1].key?(:struct)
+          struct_mappings[mapping[1][:name]] = mapping[1][:struct]
+        end
+        if mapping[1][:array]
+          array_names << mapping[1][:name]
+        end
+      end
+
       define_singleton_method("attribute_mappings") { json_name_mappings.freeze }
       define_singleton_method("struct_mappings") { struct_mappings.freeze }
+      define_singleton_method("array_names") { array_names.freeze }
 
       json_name_mappings.each do |json_name, attr_name|
         attr_accessor attr_name
@@ -18,12 +46,22 @@ module OrcaApi
       attributes.each do |k, v|
         k = k.to_s
         attr_name = self.class.attribute_mappings[k] || k
-        if self.class.struct_mappings.key?(attr_name) && v.is_a?(Hash)
-          v = self.class.struct_mappings[attr_name].new(v)
-        end
+        value = (
+          if v && self.class.struct_mappings.key?(attr_name)
+            if self.class.array_names.include?(attr_name)
+              v.map { |attrs|
+                self.class.struct_mappings[attr_name].new(attrs)
+              }
+            else
+              self.class.struct_mappings[attr_name].new(v)
+            end
+          else
+            v
+          end
+        )
         setter_name = "#{attr_name}="
         if respond_to?(setter_name)
-          send(setter_name, v)
+          send(setter_name, value)
         end
       end
     end
--- lib/orca_api/patient_information.rb
+++ lib/orca_api/patient_information.rb
@@ -8,94 +8,96 @@ module OrcaApi
     # 患者情報に含まれる自宅情報を表現するクラス
     class HomeAddressInformation < ApiStruct
       define_accessors(
-        "Address_ZipCode" => "address_zip_code",
-        "WholeAddress1" => "whole_address1",
-        "WholeAddress2" => "whole_address2",
-        "PhoneNumber1" => "phone_number1",
-        "PhoneNumber2" => "phone_number2"
+        %w(
+          Address_ZipCode
+          WholeAddress1
+          WholeAddress2
+          PhoneNumber1
+          PhoneNumber2
+        )
       )
     end
 
     # 患者情報に含まれる勤務先を表現するクラス
     class WorkPlaceInformation < ApiStruct
       define_accessors(
-        "WholeName" => "whole_name",
-        "Address_ZipCode" => "address_zip_code",
-        "WholeAddress1" => "whole_address1",
-        "WholeAddress2" => "whole_address2",
-        "PhoneNumber" => "phone_number"
+        %w(
+          WholeName
+          Address_ZipCode
+          WholeAddress1
+          WholeAddress2
+          PhoneNumber
+        )
       )
     end
 
     # 患者情報に含まれる連絡先を表現するクラス
     class ContactInformation < ApiStruct
       define_accessors(
-        "WholeName" => "whole_name",
-        "Relationship" => "relationship",
-        "Address_ZipCode" => "address_zip_code",
-        "WholeAddress1" => "whole_address1",
-        "WholeAddress2" => "whole_address2",
-        "PhoneNumber1" => "phone_number1",
-        "PhoneNumber2" => "phone_number2"
+        %w(
+          WholeName
+          Relationship
+          Address_ZipCode
+          WholeAddress1
+          WholeAddress2
+          PhoneNumber1
+          PhoneNumber2
+        )
       )
     end
 
     # 患者情報に含まれる帰省先を表現するクラス
     class Home2Information < ApiStruct
       define_accessors(
-        "WholeName" => "whole_name",
-        "Address_ZipCode" => "address_zip_code",
-        "WholeAddress1" => "whole_address1",
-        "WholeAddress2" => "whole_address2",
-        "PhoneNumber" => "phone_number"
+        %w(
+          WholeName
+          Address_ZipCode
+          WholeAddress1
+          WholeAddress2
+          PhoneNumber
+        )
       )
     end
 
     define_accessors(
-      {
-        "Patient_ID" => "patient_id",
-        "WholeName" => "whole_name",
-        "WholeName_inKana" => "whole_name_in_kana",
-        "BirthDate" => "birth_date",
-        "Sex" => "sex",
-        "HouseHolder_WholeName" => "house_holder_whole_name",
-        "Relationship" => "relationship",
-        "Occupation" => "occupation",
-        "NickName" => "nick_name",
-        "CellularNumber" => "cellular_number",
-        "FaxNumber" => "fax_number",
-        "EmailAddress" => "email_address",
-        "Home_Address_Information" => "home_address_information",
-        "WorkPlace_Information" => "work_place_information",
-        "Contact_Information" => "contact_information",
-        "Home2_Information" => "home2_information",
-        "Contraindication1" => "contraindication1",
-        "Contraindication2" => "contraindication2",
-        "Allergy1" => "allergy1",
-        "Allergy2" => "allergy2",
-        "Infection1" => "infection1",
-        "Infection2" => "infection2",
-        "Comment1" => "comment1",
-        "Comment2" => "comment2",
-        "TestPatient_Flag" => "test_patient_flag",
-        "Death_Flag" => "death_flag",
-        "Reduction_Reason" => "reduction_reason",
-        "Reduction_Reason_Name" => "reduction_reason_name",
-        "Discount" => "discount",
-        "Discount_Name" => "discount_name",
-        "Condition1" => "condition1",
-        "Condition1_Name" => "condition1_name",
-        "Condition2" => "condition2",
-        "Condition2_Name" => "condition2_name",
-        "Condition3" => "condition3",
-        "Condition3_Name" => "condition3_name"
-      },
-      {
-        "home_address_information" => HomeAddressInformation,
-        "work_place_information" => WorkPlaceInformation,
-        "contact_information" => ContactInformation,
-        "home2_information" => Home2Information,
-      }
+      [
+        "Patient_ID",
+        "WholeName",
+        "WholeName_inKana",
+        "BirthDate",
+        "Sex",
+        "HouseHolder_WholeName",
+        "Relationship",
+        "Occupation",
+        "NickName",
+        "CellularNumber",
+        "FaxNumber",
+        "EmailAddress",
+        ["Home_Address_Information", { struct: HomeAddressInformation }],
+        ["WorkPlace_Information", { struct: WorkPlaceInformation }],
+        ["Contact_Information", { struct: ContactInformation }],
+        ["Home2_Information", { struct: Home2Information }],
+        "Contraindication1",
+        "Contraindication2",
+        "Allergy1",
+        "Allergy2",
+        "Infection1",
+        "Infection2",
+        "Comment1",
+        "Comment2",
+        "TestPatient_Flag",
+        "Death_Flag",
+        "Reduction_Reason",
+        "Reduction_Reason_Name",
+        "Discount",
+        "Discount_Name",
+        "Condition1",
+        "Condition1_Name",
+        "Condition2",
+        "Condition2_Name",
+        "Condition3",
+        "Condition3_Name",
+      ]
     )
     alias_method("id", "patient_id")
     alias_method("id=", "patient_id=")
