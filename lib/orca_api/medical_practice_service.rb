45a3c536dea19fab01d419cbe669a2fd1837677a
Kouji Takao <kouji@netlab.jp>
2017-08-17 07:39:50 +0900

    refactor.

--- lib/orca_api/medical_practice_service.rb
+++ lib/orca_api/medical_practice_service.rb
@@ -7,8 +7,8 @@ module OrcaApi
   # 診療行為を扱うサービスを表現したクラス
   class MedicalPracticeService < Service
     # 診察料情報の取得
-    def get_examination_fee(diagnosis)
-      res = call_request_number_01(diagnosis)
+    def get_examination_fee(params)
+      res = call_request_number_01(params)
       if res.ok?
         unlock(res)
       end
@@ -16,34 +16,34 @@ module OrcaApi
     end
 
     # 診療情報及び請求情報の取得
-    def calc_medical_practice_fee(diagnosis)
-      res = call_request_number_01(diagnosis)
+    def calc_medical_practice_fee(params)
+      res = call_request_number_01(params)
       if res.ok?
         locked_result = res
       else
         return res
       end
 
-      calc_medical_practice_fee_without_unlock(diagnosis, res)
+      calc_medical_practice_fee_without_unlock(params, res)
     ensure
       unlock(locked_result)
     end
 
     # 診療行為の登録
-    def create(diagnosis)
-      res = call_request_number_01(diagnosis)
+    def create(params)
+      res = call_request_number_01(params)
       if res.ok?
         locked_result = res
       else
         return res
       end
 
-      res = calc_medical_practice_fee_without_unlock(diagnosis, res)
+      res = calc_medical_practice_fee_without_unlock(params, res)
       if !res.ok?
         return res
       end
 
-      res = call_request_number_05(diagnosis, res)
+      res = call_request_number_05(params, res)
       if res.ok?
         locked_result = nil
       end
@@ -65,22 +65,22 @@ module OrcaApi
 
     private
 
-    def call_request_number_01(diagnosis)
+    def call_request_number_01(params)
       body = {
         "medicalv3req1" => {
           "Request_Number" => "01",
           "Karte_Uid" => orca_api.karte_uid,
-          "Patient_ID" => diagnosis["Patient_ID"],
-          "Perform_Date" => diagnosis["Perform_Date"],
-          "Perform_Time" => diagnosis["Perform_Time"],
+          "Patient_ID" => params["Patient_ID"],
+          "Perform_Date" => params["Perform_Date"],
+          "Perform_Time" => params["Perform_Time"],
           "Orca_Uid" => "",
-          "Diagnosis_Information" => diagnosis["Diagnosis_Information"],
+          "Diagnosis_Information" => params["Diagnosis_Information"],
         },
       }
       Result.new(orca_api.call("/api21/medicalmodv31", body: body))
     end
 
-    def call_request_number_02(diagnosis, previous_result)
+    def call_request_number_02(params, previous_result)
       res = previous_result
       body = {
         "medicalv3req2" => {
@@ -94,10 +94,10 @@ module OrcaApi
             "Department_Code" => res.body["Department_Code"],
             "Physician_Code" => res.body["Physician_Code"],
             "HealthInsurance_Information" => res.patient_information["HealthInsurance_Information"],
-            "Outside_Class" => diagnosis["Diagnosis_Information"]["Outside_Class"],
+            "Outside_Class" => params["Diagnosis_Information"]["Outside_Class"],
             "Medical_OffTime" => res.body["Medical_OffTime"],
             "Medical_Information" => {
-              "Medical_Info" => diagnosis["Diagnosis_Information"]["Medical_Information"]["Medical_Info"],
+              "Medical_Info" => params["Diagnosis_Information"]["Medical_Information"]["Medical_Info"],
             }
           },
         },
@@ -105,7 +105,7 @@ module OrcaApi
       Result.new(orca_api.call("/api21/medicalmodv32", body: body))
     end
 
-    def call_request_number_03(_diagnosis, previous_result, answer = nil)
+    def call_request_number_03(previous_result, answer = nil)
       res = previous_result
       body = {
         "medicalv3req2" => {
@@ -123,11 +123,11 @@ module OrcaApi
       Result.new(orca_api.call("/api21/medicalmodv32", body: body))
     end
 
-    def call_request_number_04(diagnosis, previous_result)
+    def call_request_number_04(params, previous_result)
       res = previous_result
 
       can_delete = res.medical_information["Medical_Info"].any? { |i| i["Medical_Delete_Number"] }
-      if can_delete && !diagnosis["Delete_Number_Info"]
+      if can_delete && !params["Delete_Number_Info"]
         return EmptyDeleteNumberInfoError.new(res.raw)
       end
 
@@ -135,56 +135,56 @@ module OrcaApi
         "medicalv3req3" => {
           "Request_Number" => res.response_number,
           "Karte_Uid" => res.karte_uid,
-          "Base_Date" => diagnosis["Base_Date"],
+          "Base_Date" => params["Base_Date"],
           "Patient_ID" => res.patient_information["Patient_ID"],
           "Perform_Date" => res.body["Perform_Date"],
           "Orca_Uid" => res.orca_uid,
-          "Medical_Mode" => (can_delete && diagnosis["Delete_Number_Info"] ? "1" : nil),
-          "Delete_Number_Info" => diagnosis["Delete_Number_Info"],
-          "Ic_Code" => diagnosis["Ic_Code"],
-          "Ic_Money" => diagnosis["Ic_Money"],
-          "Ad_Money1" => diagnosis["Ad_Money1"],
-          "Ad_Money2" => diagnosis["Ad_Money2"],
+          "Medical_Mode" => (can_delete && params["Delete_Number_Info"] ? "1" : nil),
+          "Delete_Number_Info" => params["Delete_Number_Info"],
+          "Ic_Code" => params["Ic_Code"],
+          "Ic_Money" => params["Ic_Money"],
+          "Ad_Money1" => params["Ad_Money1"],
+          "Ad_Money2" => params["Ad_Money2"],
         },
       }
       Result.new(orca_api.call("/api21/medicalmodv33", body: body))
     end
 
-    def call_request_number_05(diagnosis, previous_result)
+    def call_request_number_05(params, previous_result)
       res = previous_result
       body = {
         "medicalv3req3" => {
           "Request_Number" => res.response_number,
           "Karte_Uid" => res.karte_uid,
-          "Base_Date" => diagnosis["Base_Date"],
+          "Base_Date" => params["Base_Date"],
           "Patient_ID" => res.patient_information["Patient_ID"],
           "Perform_Date" => res.body["Perform_Date"],
           "Orca_Uid" => res.orca_uid,
-          "Ic_Code" => diagnosis["Ic_Code"],
-          "Ic_Money" => diagnosis["Ic_Money"],
-          "Ad_Money1" => diagnosis["Ad_Money1"],
-          "Ad_Money2" => diagnosis["Ad_Money2"],
+          "Ic_Code" => params["Ic_Code"],
+          "Ic_Money" => params["Ic_Money"],
+          "Ad_Money1" => params["Ad_Money1"],
+          "Ad_Money2" => params["Ad_Money2"],
         },
       }
       Result.new(orca_api.call("/api21/medicalmodv33", body: body))
     end
 
-    def calc_medical_practice_fee_without_unlock(diagnosis, get_examination_fee_result)
-      res = call_request_number_02(diagnosis, get_examination_fee_result)
+    def calc_medical_practice_fee_without_unlock(params, get_examination_fee_result)
+      res = call_request_number_02(params, get_examination_fee_result)
       if !res.ok?
         return res
       end
 
-      res = call_request_number_03(diagnosis, res)
+      res = call_request_number_03(res)
       while !res.ok?
         if res.body["Medical_Select_Flag"] == "True"
-          if diagnosis["Medical_Select_Information"]
-            answer = diagnosis["Medical_Select_Information"].find { |i|
+          if params["Medical_Select_Information"]
+            answer = params["Medical_Select_Information"].find { |i|
               i["Medical_Select"] == res.body["Medical_Select_Information"]["Medical_Select"] && i["Select_Answer"]
             }
           end
           if answer
-            res = call_request_number_03(diagnosis, res, answer)
+            res = call_request_number_03(res, answer)
           else
             return UnselectedError.new(res.raw)
           end
@@ -193,7 +193,7 @@ module OrcaApi
         end
       end
 
-      call_request_number_04(diagnosis, res)
+      call_request_number_04(params, res)
     end
 
     def unlock(locked_result)
