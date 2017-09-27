beeff94780ffbf5efa8568aed9e19c5f34e9de74
Yuki MIYAUCHI <yuki.miyauchi@medley.jp>
2017-09-27 11:48:58 +0900

    エラーレスポンスとそうでないレスポンスでcontextを分けた

--- spec/orca_api/form_data_service_spec.rb
+++ spec/orca_api/form_data_service_spec.rb
@@ -7,29 +7,22 @@ RSpec.describe OrcaApi::FormDataService, orca_api_mock: true do
   describe "#get" do
     let(:data_id) { SecureRandom.hex }
 
-    context "正しいData IDを指定した場合" do
-      let(:response_json) { load_orca_api_response_json("api01rv2_formdatagetv2_00.json", false) }
+    subject { service.get data_id }
 
-      before do
-        expect(orca_api).to receive(:call).with("/api01rv2/formdatagetv2",
-                                                body: { "data" => { "Data_ID" => data_id } }).once.and_return(response_json)
-      end
-
-      subject { service.get data_id }
-
-      its("ok?") { is_expected.to be true }
-      its("forms") { is_expected.to_not be_empty }
+    before do
+      expect(orca_api).to receive(:call).with("/api01rv2/formdatagetv2",
+                                              body: { "data" => { "Data_ID" => data_id } }).once.and_return(response_json)
     end
 
-    context "不正なData IDを指定した場合" do
-      let(:response_json) { load_orca_api_response_json("api01rv2_formdatagetv2_01.json", false) }
+    context "正常系" do
+      let(:response_json) { load_orca_api_response_json("api01rv2_formdatagetv2.json", false) }
 
-      before do
-        expect(orca_api).to receive(:call).with("/api01rv2/formdatagetv2",
-                                                body: { "data" => { "Data_ID" => data_id } }).once.and_return(response_json)
-      end
+      its("ok?") { is_expected.to be true }
+      its("forms") { is_expected.to eq response_json["Forms"] }
+    end
 
-      subject { service.get data_id }
+    context "エラーレスポンスの場合" do
+      let(:response_json) { load_orca_api_response_json("api01rv2_formdatagetv2_0001.json", false) }
 
       its("ok?") { is_expected.to be false }
     end
