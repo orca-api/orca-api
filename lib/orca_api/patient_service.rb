5a71b97e3dc9e489a03bdb3366353ec13153af47
Kouji Takao <kouji@netlab.jp>
2017-09-30 01:16:51 +0900

    body instead @body .
    must use body, because @body is nil before call body.

--- lib/orca_api/patient_service.rb
+++ lib/orca_api/patient_service.rb
@@ -10,7 +10,7 @@ module OrcaApi
     # 患者情報の登録の結果を表現するクラス
     class CreateResult < ::OrcaApi::Result
       def duplicated_patient_candidates
-        @body["Patient2_Information"] || []
+        body["Patient2_Information"] || []
       end
     end
 
