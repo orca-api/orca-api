6cb7aa591035f873fb1f92536ba8b2a9a51193b9
Kouji Takao <kouji@netlab.jp>
2017-10-17 15:16:25 +0900

    IncomeService#{pay_back,recalculate,bulk_recalculate,bulk_update,destroy}の実装 #88

--- lib/orca_api/income_service.rb
+++ lib/orca_api/income_service.rb
@@ -38,28 +38,62 @@ module OrcaApi
     #
     # @see http://cms-edit.orca.med.or.jp/_admin/preview_revision/19667#api3
     def update(args)
-      update_process(args, "01")
+      common_update_process(args, "01")
     end
 
     # 履歴修正
     #
     # @see http://cms-edit.orca.med.or.jp/_admin/preview_revision/19667#api4
     def update_history(args)
-      update_process(args, "02")
+      common_update_process(args, "02")
     end
 
     # 入金取消
     #
     # @see http://cms-edit.orca.med.or.jp/_admin/preview_revision/19667#api5
     def cancel(args)
-      update_process(args, "03")
+      common_update_process(args, "03")
+    end
+
+    # 返金
+    #
+    # @see http://cms-edit.orca.med.or.jp/_admin/preview_revision/19667#api6
+    def pay_back(args)
+      common_update_process(args, "04")
+    end
+
+    # 再計算
+    #
+    # @see http://cms-edit.orca.med.or.jp/_admin/preview_revision/19667#api7
+    def recalculate(args)
+      common_update_process(args, "05")
+    end
+
+    # 一括再計算
+    #
+    # @see http://cms-edit.orca.med.or.jp/_admin/preview_revision/19667#api8
+    def bulk_recalculate(args)
+      common_update_process(args, "06")
+    end
+
+    # 一括入返金
+    #
+    # @see http://cms-edit.orca.med.or.jp/_admin/preview_revision/19667#api9
+    def bulk_update(args)
+      common_update_process(args, "07")
+    end
+
+    # 請求取消
+    #
+    # @see http://cms-edit.orca.med.or.jp/_admin/preview_revision/19667#api10
+    def destroy(args)
+      common_update_process(args, "08")
     end
 
     private
 
-    # 更新処理の共通処理
-    def update_process(args, mode)
-      res = call_01("02", args)
+    def common_update_process(args, mode)
+      res = lock(args)
       if !res.locked?
         locked_result = res
       end
@@ -69,6 +103,23 @@ module OrcaApi
       call_02(mode, args, res)
     ensure
       unlock(locked_result)
+    end
+
+    def lock(args)
+      if args.key?("InOut") && args.key?("Invoice_Number")
+        mode = "02"
+      else
+        mode = "01"
+        args = args.merge(
+          "Information_Class" => "1",
+          "Start_Month" => "0000-01",
+          "Selection" => {
+            "First" => "1",
+            "Last" => "1",
+          }
+        )
+      end
+      call_01(mode, args)
     end
 
     def call_01(mode, args)
@@ -87,10 +138,12 @@ module OrcaApi
         "Request_Mode" => mode,
         "Karte_Uid" => orca_api.karte_uid,
         "Orca_Uid" => res.orca_uid,
-        "Patient_ID" => res["Patient_ID"],
-        "InOut" => res["Income_Detail"]["InOut"],
-        "Invoice_Number" => res["Income_Detail"]["Invoice_Number"]
+        "Patient_ID" => res["Patient_ID"]
       )
+      if (income_detail = res["Income_Detail"])
+        req["InOut"] = income_detail["InOut"]
+        req["Invoice_Number"] = income_detail["Invoice_Number"]
+      end
       Result.new(orca_api.call(PATH, body: { REQUEST_NAME => req }))
     end
 
