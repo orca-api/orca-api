c7e34192cb6af7b036638bba87b1abc478361d65
Kouji Takao <kouji@netlab.jp>
2017-11-17 11:35:19 +0900

    OrcaApi::OrcaApi.newの引数の変更 #100

--- lib/orca_api/orca_api.rb
+++ lib/orca_api/orca_api.rb
@@ -3,9 +3,6 @@ require "net/http"
 require "json"
 require "securerandom"
 
-require_relative "orca_api/ssl_client_authentication"
-require_relative "orca_api/basic_authentication"
-
 require_relative "result"
 require_relative "form_result"
 
@@ -13,8 +10,15 @@ module OrcaApi
   # 日医標準レセプトソフト APIを呼び出すため低レベルインタフェースを提供するクラス
   class OrcaApi
     attr_accessor :host
-    attr_accessor :authentication
     attr_accessor :port
+    attr_accessor :user
+    attr_accessor :password
+    attr_accessor :use_ssl
+    attr_accessor :ca_file
+    attr_accessor :ca_path
+    attr_accessor :verify_mode
+    attr_accessor :cert
+    attr_accessor :key
     attr_writer :karte_uid
     attr_accessor :debug_output
 
@@ -25,10 +29,16 @@ module OrcaApi
         downcase
     end
 
-    def initialize(host, authentication, port = 8000)
-      @host = host
-      @authentication = authentication
-      @port = port
+    def initialize(uri, options = {})
+      uri = URI.parse(uri)
+      @host = uri.host
+      @port = uri.port
+      @user = uri.user || options[:user]
+      @password = uri.password || options[:password]
+      @use_ssl = uri.scheme == 'https' || options[:use_ssl]
+      if @use_ssl
+        extract_ssl_options(options.fetch(:ssl))
+      end
     end
 
     def karte_uid
@@ -36,35 +46,9 @@ module OrcaApi
     end
 
     def call(path, params: {}, body: nil, http_method: :post)
-      case http_method
-      when :get
-        request_class = Net::HTTP::Get
-      when :post
-        request_class = Net::HTTP::Post
-      end
-
-      query = params.merge(format: "json").map { |k, v|
-        "#{k}=#{v}"
-      }.join("&")
-
-      req = request_class.new("#{path}?#{query}")
-
-      if body
-        req.body = body.to_json
-      end
-
-      http = Net::HTTP.new(@host, @port)
-
-      if @debug_output
-        http.set_debug_output(@debug_output)
-      end
-
-      [@authentication].flatten.each do |auth|
-        auth.apply(http, req)
-      end
-
-      http.start { |h|
-        res = h.request(req)
+      req = make_request(http_method, path, params, body)
+      new_http.start { |http|
+        res = http.request(req)
         JSON.parse(res.body)
       }
     end
@@ -90,6 +74,79 @@ module OrcaApi
       define_method("new_#{s}") do
         service_class.new(self)
       end
+    end
+
+    private
+
+    def extract_ssl_options(ssl)
+      @ca_file = ssl[:ca_file]
+      @ca_path = ssl[:ca_path]
+
+      @verify_mode = ssl.fetch(:verify_mode) do
+        if ssl.fetch(:verify, true)
+          OpenSSL::SSL::VERIFY_PEER
+        else
+          OpenSSL::SSL::VERIFY_NONE
+        end
+      end
+
+      if (p12 = ssl[:p12])
+        @cert = p12.certificate
+        @key = p12.key
+      else
+        @cert = ssl[:cert]
+        @key = ssl[:key]
+      end
+    end
+
+    def new_http
+      http = Net::HTTP.new(@host, @port)
+
+      if @use_ssl
+        http.use_ssl = true
+        if @cert
+          http.cert = @cert
+        end
+        if @key
+          http.key = @key
+        end
+        if @ca_file
+          http.ca_file = @ca_file
+        end
+        if @ca_path
+          http.ca_path = @ca_path
+        end
+        if @verify_mode
+          http.verify_mode = @verify_mode
+        end
+      end
+
+      if @debug_output
+        http.set_debug_output(@debug_output)
+      end
+
+      http
+    end
+
+    def make_request(http_method, path, params, body)
+      case http_method
+      when :get
+        request_class = Net::HTTP::Get
+      when :post
+        request_class = Net::HTTP::Post
+      end
+
+      query = URI.encode_www_form(params.merge(format: "json"))
+
+      req = request_class.new("#{path}?#{query}")
+
+      req.basic_auth(@user, @password)
+
+      if body
+        req.body = body.to_json
+      end
+
+      req
     end
   end
 end
