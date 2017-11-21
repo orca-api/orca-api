a62d4c0963b06f6b055fbcb0e2cea1b6c330b9ed
Kouji Takao <kouji@netlab.jp>
2017-11-21 14:00:37 +0900

    OrcaApi::OrcaApi#new_image_serviceのテストを追加

--- spec/orca_api/orca_api_spec.rb
+++ spec/orca_api/orca_api_spec.rb
@@ -291,6 +291,7 @@ RSpec.describe OrcaApi::OrcaApi do
     ["new_form_data_service", OrcaApi::FormDataService],
     ["new_income_service", OrcaApi::IncomeService],
     ["new_print_service", OrcaApi::PrintService],
+    ["new_image_service", OrcaApi::ImageService],
   ].each do |method_name, service_class|
     describe "##{method_name}" do
       subject { orca_api.send(method_name) }
