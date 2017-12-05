b2f0ff5a325daa9b212dfe07cd3af2d4d6be25e7
Kouji Takao <kouji@netlab.jp>
2017-12-05 15:47:08 +0900

    OrcaApi::ReceiptService#createdの実装の修正と自動テストの追加 #106
    戻り値に処理中を示す doing? メソッドを追加するため、Resultクラスから派生した
    CheckResultクラスを用意した。

--- lib/orca_api/receipt_service.rb
+++ lib/orca_api/receipt_service.rb
@@ -11,13 +11,23 @@ module OrcaApi
   #      * 作成が完了するまで定期的に(1)-2を繰り返す
   #  * (2) レセプト印刷
   #    * (2)-1 印刷指示: print
-  #    * (2)-2 印刷確認: printed
+  #    * (2)-2 印刷結果確認: printed
   #      * 印刷(PDFの生成)が完了するまで定期的に(2)-2を繰り返す
   #  * (3) PDF(大容量データ)の取得
   #    * 大容量データを扱うサービス BlobService を使用して、生成したPDFを取得する
   #
   # @see http://cms-edit.orca.med.or.jp/_admin/preview_revision/19238
   class ReceiptService < Service
+    # 作成確認と印刷結果確認に共通の確認結果を扱うクラス
+    class CheckResult < ::OrcaApi::Result
+      # 処理中かどうか
+      #
+      # @return [Boolean] 処理中であればtrue、そうでなければfalse。
+      def doing?
+        api_result == "E70"
+      end
+    end
+
     # レセプト作成:作成指示
     #
     # @param [Hash] args
@@ -60,6 +70,31 @@ module OrcaApi
       Result.new(orca_api.call("/orca42/receiptmakev3", body: { "receipt_makev3req" => req }))
     end
 
+    # レセプト作成:作成確認
+    #
+    # @param [Hash] args
+    #   * "Orca_Uid" (String)
+    #     オルカUID。必須。
+    #   * "Perform_Date" (String)
+    #     実施年月日。YYYY-mm-dd形式。
+    #   * "Perform_Month" (String)
+    #     診療年月。処理区分がAll のとき必須。
+    #   * "InOut" (String)
+    #     入外区分。必須。I：入院、O：入院外。
+    #   * "Receipt_Mode" (String)
+    #     処理区分。All：一括作成　All以外：個別作成。
+    #   * "Print_Mode" (String)
+    #     印刷モード。Check：点検用　Check以外：提出用。
+    #     個別作成でCheck(点検用)のとき、点検用は平成２０年４月診療分から対応のため、
+    #     診療年月が平成２０年３月以前の場合は提出用として作成します。
+    #   * "Submission_Mode" (String)
+    #     提出先。必須。
+    #     医保 01:全件 02:社保 03:国保 04:広域 労災 05 自賠責 06:新様式 07:従来様式 08:第三者行為 公害 09。
+    # @return [OrcaApi::CheckResult]
+    #   日レセからのレスポンス
+    #
+    # @see http://cms-edit.orca.med.or.jp/receipt/tec/api/haori_receipt.data/receiptmakev3.pdf
+    # @see http://cms-edit.orca.med.or.jp/receipt/tec/api/haori_receipt.data/receiptmakev3_err.pdf
     def created(args)
       req = args.merge(
         {
@@ -68,7 +103,7 @@ module OrcaApi
         }
       )
 
-      Result.new(orca_api.call("/orca42/receiptmakev3", body: { "receipt_makev3req" => req }))
+      CheckResult.new(orca_api.call("/orca42/receiptmakev3", body: { "receipt_makev3req" => req }))
     end
 
     def print(args)
@@ -90,7 +125,7 @@ module OrcaApi
         }
       )
 
-      Result.new(orca_api.call("/orca42/receiptprintv3", body: { "receipt_printv3req" => req }))
+      CheckResult.new(orca_api.call("/orca42/receiptprintv3", body: { "receipt_printv3req" => req }))
     end
   end
 end
--- spec/orca_api/receipt_service_spec.rb
+++ spec/orca_api/receipt_service_spec.rb
@@ -3,7 +3,8 @@ require_relative "shared_examples"
 
 RSpec.describe OrcaApi::ReceiptService, orca_api_mock: true do
   let(:service) { described_class.new(orca_api) }
-  let(:response_data) { parse_json(response_json, false) }
+  let(:orca_uid) { "c585dc3e-fa42-4f45-b02f-5a4166d0721d" }
+  let(:response_data) { parse_json(response_json) }
 
   describe "#create" do
     let(:args) {
@@ -13,7 +14,7 @@ RSpec.describe OrcaApi::ReceiptService, orca_api_mock: true do
         "InOut" => "O",
         "Receipt_Mode" => "All",
         "Print_Mode" => "Check",
-        "Submission_Mode" => "02",
+        "Submission_Mode" => "01",
         "Patient_Information" => [],
       }
     }
@@ -59,11 +60,96 @@ RSpec.describe OrcaApi::ReceiptService, orca_api_mock: true do
 
       its("ok?") { is_expected.to be(true) }
       its(["Response_Number"]) { is_expected.to eq("02") }
-      its(["Orca_Uid"]) { is_expected.to eq("c585dc3e-fa42-4f45-b02f-5a4166d0721d") }
+      its(["Orca_Uid"]) { is_expected.to eq(orca_uid) }
     end
 
     context "異常系" do
       let(:response_json) { load_orca_api_response("orca42_receiptmakev3_01_E13.json") }
+
+      its("ok?") { is_expected.to be(false) }
+    end
+  end
+
+  describe "#created" do
+    let(:args) {
+      {
+        "Orca_Uid" => orca_uid,
+        "Perform_Date" => "",
+        "Perform_Month" => "2017-11",
+        "InOut" => "O",
+        "Receipt_Mode" => "All",
+        "Print_Mode" => "Check",
+        "Submission_Mode" => "01",
+        "Patient_Information" => [],
+      }
+    }
+
+    subject { service.created(args) }
+
+    before do
+      count = 0
+      prev_response_json = nil
+      expect(orca_api).to receive(:call).exactly(1) { |path, body:|
+        count += 1
+        prev_response_json =
+          case count
+          when 1
+            aggregate_failures "リクエスト内容のチェック" do
+              expect(path).to eq("/orca42/receiptmakev3")
+
+              req = body["receipt_makev3req"]
+              expect(req["Request_Number"]).to eq("02")
+              expect(req["Karte_Uid"]).to eq(orca_api.karte_uid)
+              %w(
+                Orca_Uid
+                Perform_Date
+                Perform_Month
+                InOut
+                Receipt_Mode
+                Print_Mode
+                Submission_Mode
+                Patient_Information
+              ).each do |name|
+                expect(req[name]).to eq(args[name])
+              end
+            end
+
+            response_json
+          end
+        prev_response_json
+      }
+    end
+
+    context "正常系" do
+      context "処理中" do
+        let(:response_json) { load_orca_api_response("orca42_receiptmakev3_02_E70.json") }
+
+        its("ok?") { is_expected.to be(false) }
+        its("doing?") { is_expected.to be(true) }
+        its(["Response_Number"]) { is_expected.to eq("01") }
+        its(["Orca_Uid"]) { is_expected.to eq(orca_uid) }
+      end
+
+      context "完了" do
+        let(:response_json) { load_orca_api_response("orca42_receiptmakev3_02.json") }
+
+        its("ok?") { is_expected.to be(true) }
+        its("doing?") { is_expected.to be(false) }
+        its(["Response_Number"]) { is_expected.to eq("02") }
+        its(["Orca_Uid"]) { is_expected.to eq(orca_uid) }
+
+        %w(
+          All_Count
+          All_Number_Of_Sheets
+          Receipt_Information
+        ).each do |name|
+          its([name]) { is_expected.to eq(response_data.first[1][name]) }
+        end
+      end
+    end
+
+    context "異常系" do
+      let(:response_json) { load_orca_api_response("orca42_receiptmakev3_02_E41.json") }
 
       its("ok?") { is_expected.to be(false) }
     end
