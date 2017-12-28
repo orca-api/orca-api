4896bd07b9333e30ceb2d4f25b5cdbe78f612b5f
Kouji Takao <kouji@netlab.jp>
2017-12-28 11:53:24 +0900

    LockService#unlock_allの追加 #114
    排他制御情報をすべて解除するインタフェースの追加。

--- lib/orca_api/lock_service.rb
+++ lib/orca_api/lock_service.rb
@@ -5,7 +5,7 @@ module OrcaApi
   #
   # @see http://cms-edit.orca.med.or.jp/_admin/preview_revision/20796
   class LockService < Service
-    # 排他制御情報の一覧を取得する
+    # 排他制御情報の一覧を取得する。
     #
     # @return [OrcaApi::Result]
     #   日レセからのレスポンス
@@ -18,6 +18,42 @@ module OrcaApi
         "Karte_Uid" => orca_api.karte_uid,
       }
 
+      Result.new(orca_api.call("/api21/medicalmodv37", body: { "medicalv3req7" => req }))
+    end
+
+    # 排他制御情報をすべて解除する。
+    #
+    # 排他解除は残っている一時データも削除する。処理中のオルカＵＩＤの排他解除を行った場合の不具合には対応できない。
+    #
+    # また、日レセAPIの制限で、解除対象の排他時間が処理時間より１分以内の場合は解除できない。
+    # この場合のApi_Resultは「E14」、Api_Result_Messageは「端末展開中と思われる排他時間です。
+    # 端末が展開中でないことを確認して下さい。」です。排他解除が必要であれば、１分後に再度送信して下さい。
+    #
+    # @return [OrcaApi::Result]
+    #   日レセからのレスポンス
+    #
+    # @see http://cms-edit.orca.med.or.jp/receipt/tec/api/haori/lockdel.data/api02107v03.pdf
+    # @see http://cms-edit.orca.med.or.jp/receipt/tec/api/haori/lockdel.data/api02107v03_err.pdf
+    def unlock_all
+      req = {
+        "Request_Number" => "01",
+        "Karte_Uid" => orca_api.karte_uid,
+        "Delete_Information" => {
+          "Delete_Class" => "All",
+        },
+      }
+      res = Result.new(orca_api.call("/api21/medicalmodv37", body: { "medicalv3req7" => req }))
+      if res.api_result != "S40"
+        return res
+      end
+
+      req = {
+        "Request_Number" => res.response_number,
+        "Karte_Uid" => res.karte_uid,
+        "Orca_Uid" => res.orca_uid,
+        "Delete_Information" => res.delete_information,
+        "Select_Answer" => "Ok",
+      }
       Result.new(orca_api.call("/api21/medicalmodv37", body: { "medicalv3req7" => req }))
     end
   end
--- spec/orca_api/lock_service_spec.rb
+++ spec/orca_api/lock_service_spec.rb
@@ -45,4 +45,124 @@ RSpec.describe OrcaApi::LockService, orca_api_mock: true do
       its("ok?") { is_expected.to be false }
     end
   end
+
+  describe "#unlock_all" do
+    def expect_orca_api_call(expect_data)
+      count = 0
+      prev_response = nil
+      expect(orca_api).to receive(:call).exactly(expect_data.length) do |path, body:|
+        expect_datum = expect_data[count]
+        if expect_datum.key?(:path)
+          expect(path).to eq(expect_datum[:path])
+        end
+        if expect_datum.key?(:body)
+          expect_orca_api_call_body(body, expect_datum[:body])
+        end
+        if expect_datum.key?(:response)
+          prev_response = load_orca_api_response(expect_datum[:response])
+        end
+        count += 1
+        prev_response
+      end
+    end
+
+    def expect_orca_api_call_body(actual_body, expect_body)
+      case expect_body
+      when Hash
+        expect_body.each do |key, value|
+          if (md = /\A=(.*)\z/.match(key))
+            expect(actual_body[md[1]]).to eq(value)
+          else
+            expect_orca_api_call_body(actual_body[key], value)
+          end
+        end
+      when Array
+        expect_body.each.with_index do |value, index|
+          expect_orca_api_call_body(actual_body[index], value)
+        end
+        expect(actual_body.length).to eq(expect_body.length)
+      else
+        expect(actual_body).to eq(expect_body)
+      end
+    end
+
+    context "排他中" do
+      it "すべての排他制御を解除する" do
+        expect_data = [
+          {
+            path: "/api21/medicalmodv37",
+            body: {
+              "medicalv3req7" => {
+                "Request_Number" => "01",
+                "Karte_Uid" => "karte_uid",
+                "=Delete_Information" => {
+                  "Delete_Class" => "All",
+                },
+              }
+            },
+            response: "api21_medicalmodv37_01_S40.json",
+          },
+          {
+            path: "/api21/medicalmodv37",
+            body: {
+              "medicalv3req7" => {
+                "Request_Number" => "01",
+                "Karte_Uid" => "karte_uid",
+                "Orca_Uid" => "c585dc3e-fa42-4f45-b02f-5a4166d0721d",
+                "=Delete_Information" => {
+                  "Delete_Class" => "All",
+                },
+                "Select_Answer" => "Ok",
+              }
+            },
+            response: "api21_medicalmodv37_01.json",
+          },
+        ]
+        expect_orca_api_call(expect_data)
+
+        result = service.unlock_all
+
+        expect(result.ok?).to be true
+      end
+    end
+
+    context "排他中ではない" do
+      it "排他制御を解除するための日レセAPIを呼び出す" do
+        expect_data = [
+          {
+            path: "/api21/medicalmodv37",
+            body: {
+              "medicalv3req7" => {
+                "Request_Number" => "01",
+                "Karte_Uid" => "karte_uid",
+                "=Delete_Information" => {
+                  "Delete_Class" => "All",
+                },
+              }
+            },
+            response: "api21_medicalmodv37_01_S40_empty.json",
+          },
+          {
+            body: {
+              "medicalv3req7" => {
+                "Request_Number" => "01",
+                "Karte_Uid" => "karte_uid",
+                "Orca_Uid" => "c585dc3e-fa42-4f45-b02f-5a4166d0721d",
+                "=Delete_Information" => {
+                  "Delete_Class" => "All",
+                },
+                "Select_Answer" => "Ok",
+              }
+            },
+            response: "api21_medicalmodv37_01.json",
+          },
+        ]
+        expect_orca_api_call(expect_data)
+
+        result = service.unlock_all
+
+        expect(result.ok?).to be true
+      end
+    end
+  end
 end
