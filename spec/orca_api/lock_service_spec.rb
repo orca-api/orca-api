a2d8044314a0ba94c6c7a5da28d614e75c29ce09
Kouji Takao <kouji@netlab.jp>
2018-01-02 06:14:07 +0900

    一覧の自動テストでもexpect_orca_api_callを使うように修正

--- spec/orca_api/lock_service_spec.rb
+++ spec/orca_api/lock_service_spec.rb
@@ -3,48 +3,54 @@ require_relative "shared_examples"
 
 RSpec.describe OrcaApi::LockService, orca_api_mock: true do
   let(:service) { described_class.new(orca_api) }
-  let(:response_data) { parse_json(response_json) }
 
   describe "#list" do
-    subject { service.list }
-
-    before do
-      count = 0
-      prev_response_json = nil
-      expect(orca_api).to receive(:call).exactly(1) { |path, body:|
-        count += 1
-        prev_response_json =
-          case count
-          when 1
-            expect(path).to eq("/api21/medicalmodv37")
-
-            req = body["medicalv3req7"]
-            expect(req["Request_Number"]).to eq("00")
-            expect(req["Karte_Uid"]).to eq(orca_api.karte_uid)
-
-            response_json
-          end
-        prev_response_json
-      }
-    end
-
     context "排他中" do
-      let(:response_json) { load_orca_api_response("api21_medicalmodv37_00.json") }
+      it "一覧を取得できる" do
+        expect_data = [
+          {
+            path: "/api21/medicalmodv37",
+            body: {
+              "=medicalv3req7" => {
+                "Request_Number" => "00",
+                "Karte_Uid" => orca_api.karte_uid,
+              }
+            },
+            result: "api21_medicalmodv37_00.json",
+          },
+        ]
 
-      its("ok?") { is_expected.to be true }
-      %w(
-        Lock_Information
-      ).each do |name|
-        its([name]) { is_expected.to eq(response_data.first[1][name]) }
+        expect_orca_api_call(expect_data, binding)
+
+        result = service.list
+
+        expect(result.ok?).to be true
       end
     end
 
     context "排他中ではない" do
-      let(:response_json) { load_orca_api_response("api21_medicalmodv37_00_E10.json") }
+      it "エラーにはならず、空の一覧を取得できる" do
+        expect_data = [
+          {
+            path: "/api21/medicalmodv37",
+            body: {
+              "=medicalv3req7" => {
+                "Request_Number" => "00",
+                "Karte_Uid" => orca_api.karte_uid,
+              }
+            },
+            result: "api21_medicalmodv37_00_E10.json",
+          },
+        ]
 
-      its("ok?") { is_expected.to be true }
-      its(["Lock_Information"]) { is_expected.to eq([]) }
-      its(:lock_information) { is_expected.to eq([]) }
+        expect_orca_api_call(expect_data, binding)
+
+        result = service.list
+
+        expect(result.ok?).to be true
+        expect(result.lock_information).to eq([])
+        expect(result["Lock_Information"]).to eq([])
+      end
     end
   end
 
