c397931e5cf2dc4ae44b8025411ef777fa0b9ca2
Kouji Takao <kouji@netlab.jp>
2018-01-05 15:00:04 +0900

    RehabilitationCommentService#getの追加

--- lib/orca_api/rehabilitation_comment_service.rb
+++ lib/orca_api/rehabilitation_comment_service.rb
@@ -22,5 +22,32 @@ module OrcaApi
 
       Result.new(orca_api.call("/api21/medicalmodv35", body: { "medicalv3req5" => req }))
     end
+
+    # 該当患者のリハビリコメント詳細情報を取得する
+    #
+    # @params [String] patient_id
+    #   患者番号
+    # @params [String] medication_code
+    #   診療コード
+    # @params [String] perform_date
+    #   診療年月
+    # @params [String] insurance_combination_number
+    #   保険組合せ
+    # @return [OrcaApi::Result]
+    #   日レセからのレスポンス
+    def get(patient_id, medication_code, perform_date, insurance_combination_number)
+      req = {
+        "Request_Number" => "00",
+        "Karte_Uid" => orca_api.karte_uid,
+        "Patient_ID" => patient_id,
+        "Perform_Information" => {
+          "Medication_Code" => medication_code,
+          "Perform_Date" => perform_date,
+          "Insurance_Combination_Number" => insurance_combination_number,
+        },
+      }
+
+      Result.new(orca_api.call("/api21/medicalmodv35", body: { "medicalv3req5" => req }))
+    end
   end
 end
--- spec/orca_api/rehabilitation_comment_service_spec.rb
+++ spec/orca_api/rehabilitation_comment_service_spec.rb
@@ -16,13 +16,42 @@ RSpec.describe OrcaApi::RehabilitationCommentService, orca_api_mock: true do
               "Patient_ID" => "5",
             }
           },
-          result: "api21_medicalmodv35_00.json",
+          result: "api21_medicalmodv35_list_00.json",
         },
       ]
 
       expect_orca_api_call(expect_data, binding)
 
       result = service.list("5")
+
+      expect(result.ok?).to be true
+    end
+  end
+
+  describe "#get" do
+    it "詳細情報を取得できる" do
+      expect_data = [
+        {
+          path: "/api21/medicalmodv35",
+          body: {
+            "=medicalv3req5" => {
+              "Request_Number" => "00",
+              "Karte_Uid" => orca_api.karte_uid,
+              "Patient_ID" => "5",
+              "Perform_Information" => {
+                "Medication_Code" => "099830101",
+                "Perform_Date" => "2013-01",
+                "Insurance_Combination_Number" => "",
+              },
+            }
+          },
+          result: "api21_medicalmodv35_get_00.json",
+        },
+      ]
+
+      expect_orca_api_call(expect_data, binding)
+
+      result = service.get("5", "099830101", "2013-01", "")
 
       expect(result.ok?).to be true
     end
