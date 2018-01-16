c8ab1887ce913be8c7b91a323df80c30c4e7d54d
Yuki MIYAUCHI <yuki.miyauchi@medley.jp>
2018-01-16 12:18:05 +0900

    医師ユーザーの追加更新削除メソッドを追加した

--- lib/orca_api/physician_service.rb
+++ lib/orca_api/physician_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative "service"
 
 module OrcaApi
@@ -20,6 +22,68 @@ module OrcaApi
         req_name => {
           "Request_Number" => "02",
           "Base_Date" => base_date,
+        }
+      }
+      Result.new(orca_api.call(api_path, body: body))
+    end
+
+    # ユーザー作成
+    #
+    # @param params [Hash] ユーザー情報
+    # @option params [String] User_Id ユーザーID
+    # @option params [String] User_Password ユーザーパスワード
+    # @option params [String] Full_Name ユーザー氏名
+    # @option params [String] Kana_Name ユーザーカナ氏名
+    #
+    # @see https://www.orca.med.or.jp/receipt/tec/api/userkanri.html#request
+    # @see https://www.orca.med.or.jp/receipt/tec/api/userkanri.html#response1
+    def create(params)
+      api_path = "/orca101/manageusersv2"
+      body = {
+        "manageusersreq" => {
+          "Request_Number" => "02",
+          "User_Information" => params.merge("Group_Number" => "1")
+        }
+      }
+      Result.new(orca_api.call(api_path, body: body))
+    end
+
+    # ユーザー更新
+    #
+    # @param user_id [String] ユーザーID
+    # @param params [Hash] ユーザー情報
+    # @option params [String] New_User_Id ユーザーID
+    # @option params [String] New_User_Password ユーザーパスワード
+    # @option params [String] New_Full_Name ユーザー氏名
+    # @option params [String] New_Kana_Name ユーザーカナ氏名
+    #
+    # @see https://www.orca.med.or.jp/receipt/tec/api/userkanri.html#request
+    # @see https://www.orca.med.or.jp/receipt/tec/api/userkanri.html#response2
+    def update(user_id, params)
+      api_path = "/orca101/manageusersv2"
+      body = {
+        "manageusersreq" => {
+          "Request_Number" => "03",
+          "User_Information" => params.merge("User_Id" => user_id)
+        }
+      }
+      Result.new(orca_api.call(api_path, body: body))
+    end
+
+    # ユーザー削除
+    #
+    # @param user_id [String] ユーザーID
+    #
+    # @see https://www.orca.med.or.jp/receipt/tec/api/userkanri.html#request
+    # @see https://www.orca.med.or.jp/receipt/tec/api/userkanri.html#response3
+    def destroy(user_id)
+      api_path = "/orca101/manageusersv2"
+      body = {
+        "manageusersreq" => {
+          "Request_Number" => "04",
+          "User_Information" => {
+            "User_Id" => user_id
+          }
         }
       }
       Result.new(orca_api.call(api_path, body: body))
--- spec/orca_api/physician_service_spec.rb
+++ spec/orca_api/physician_service_spec.rb
@@ -39,4 +39,63 @@ RSpec.describe OrcaApi::PhysicianService, orca_api_mock: true do
       include_examples "結果が正しいこと"
     end
   end
+
+  describe "#create" do
+    let(:response_json) { load_orca_api_response("orca101_manageusersv2_02.json") }
+
+    it "API呼び出しが正しいこと" do
+      expect(orca_api).to receive(:call).with("/orca101/manageusersv2",
+                                              body: {
+                                                "manageusersreq" => {
+                                                  "Request_Number" => "02",
+                                                  "User_Information" => {
+                                                    "User_Id" => "doctor",
+                                                    "Group_Number" => "1",
+                                                    "Unknown" => "Unknown",
+                                                  }
+                                                }
+                                              }).and_return(response_json)
+      service.create "User_Id" => "doctor",
+                     "Group_Number" => "9",
+                     "Unknown" => "Unknown"
+    end
+  end
+
+  describe "#update" do
+    let(:response_json) { load_orca_api_response("orca101_manageusersv2_03.json") }
+
+    it "API呼び出しが正しいこと" do
+      expect(orca_api).to receive(:call).with("/orca101/manageusersv2",
+                                              body: {
+                                                "manageusersreq" => {
+                                                  "Request_Number" => "03",
+                                                  "User_Information" => {
+                                                    "User_Id" => "doctor",
+                                                    "New_Full_Name" => "山田太郎",
+                                                    "Unknown" => "Unknown",
+                                                  }
+                                                }
+                                              }).and_return(response_json)
+      service.update "doctor",
+                     "New_Full_Name" => "山田太郎",
+                     "Unknown" => "Unknown"
+    end
+  end
+
+  describe "#destroy" do
+    let(:response_json) { load_orca_api_response("orca101_manageusersv2_04.json") }
+
+    it "API呼び出しが正しいこと" do
+      expect(orca_api).to receive(:call).with("/orca101/manageusersv2",
+                                              body: {
+                                                "manageusersreq" => {
+                                                  "Request_Number" => "04",
+                                                  "User_Information" => {
+                                                    "User_Id" => "doctor",
+                                                  }
+                                                }
+                                              }).and_return(response_json)
+      service.destroy "doctor"
+    end
+  end
 end
