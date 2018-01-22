6133ff74e5534af530d90438221d03d9e54477e1
Kouji Takao <kouji@netlab.jp>
2018-01-22 14:41:56 +0900

    異常系の自動テストを追加

--- spec/orca_api/patient_service/care_insurance_spec.rb
+++ spec/orca_api/patient_service/care_insurance_spec.rb
@@ -41,6 +41,32 @@ RSpec.describe OrcaApi::PatientService::CareInsurance, orca_api_mock: true do
         expect(result.ok?).to be true
       end
     end
+
+    context "異常系" do
+      it "患者番号に該当する患者が存在しない場合、ロック解除を行わないこと" do
+        expect_data = [
+          {
+            path: "/orca12/patientmodv36",
+            body: {
+              "=patientmodv3req6" => {
+                "Request_Number" => "01",
+                "Karte_Uid" => orca_api.karte_uid,
+                "Patient_Information" => {
+                  "Patient_ID" => "999999",
+                }
+              }
+            },
+            result: "orca12_patientmodv36_get_01_E10.json",
+          },
+        ]
+
+        expect_orca_api_call(expect_data, binding)
+
+        result = service.get(999999)
+
+        expect(result.ok?).to be false
+      end
+    end
   end
 
   describe "#update" do
@@ -135,6 +161,93 @@ RSpec.describe OrcaApi::PatientService::CareInsurance, orca_api_mock: true do
         result = service.update(1, args)
 
         expect(result.ok?).to be true
+      end
+    end
+
+    context "異常系" do
+      it "患者番号に該当する患者が存在しない場合、ロック解除を行わないこと" do
+        expect_data = [
+          {
+            path: "/orca12/patientmodv36",
+            body: {
+              "=patientmodv3req6" => {
+                "Request_Number" => "01",
+                "Karte_Uid" => orca_api.karte_uid,
+                "Patient_Information" => {
+                  "Patient_ID" => "999999",
+                }
+              }
+            },
+            result: "orca12_patientmodv36_get_01_E10.json",
+          },
+        ]
+
+        expect_orca_api_call(expect_data, binding)
+
+        result = service.update(999999, {})
+
+        expect(result.ok?).to be false
+      end
+
+      it "有効開始日 ＞ 有効終了日の場合、エラーが発生してロックを解除すること" do
+        args = {
+          "Care_Insurance_Information": {
+            "Insurance_Mode": "Modify",
+            "Care_Insurance_Info": [
+              {
+                "InsuranceProvider_Number": "1",
+                "HealthInsuredPerson_Number": "",
+                "Certificate_StartDate": "2018-03-01",
+                "Certificate_ExpiredDate": "2018-01-01"
+              }
+            ]
+          }
+        }
+
+        expect_data = [
+          {
+            path: "/orca12/patientmodv36",
+            body: {
+              "=patientmodv3req6" => {
+                "Request_Number" => "01",
+                "Karte_Uid" => orca_api.karte_uid,
+                "Patient_Information" => {
+                  "Patient_ID" => "1",
+                }
+              }
+            },
+            result: "orca12_patientmodv36_update__modify_01.json",
+          },
+          {
+            path: "/orca12/patientmodv36",
+            body: {
+              "=patientmodv3req6" => args.merge(
+                "Request_Number" => '`prev.response_number`',
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+                "Patient_Information" => "`prev.patient_information`"
+              ),
+            },
+            result: "orca12_patientmodv36_update__modify_02_E60.json",
+          },
+          {
+            path: "/orca12/patientmodv36",
+            body: {
+              "=patientmodv3req6" => {
+                "Request_Number" => "99",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+              }
+            },
+            result: "orca12_patientmodv36_99.json",
+          },
+        ]
+
+        expect_orca_api_call(expect_data, binding)
+
+        result = service.update(1, args)
+
+        expect(result.ok?).to be false
       end
     end
   end
