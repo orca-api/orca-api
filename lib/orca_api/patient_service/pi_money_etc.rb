0ed6a9495308cb52a4b7e8247c6cd100c9fae908
Kouji Takao <kouji@netlab.jp>
2018-01-26 23:08:02 +0900

    他一部負担額テーブルの全件削除・追加を実装
    具体的には、
    OrcaApi::PatientService#update_pi_money_etc
    を追加。
    ただし、のちほど、 pi_money_etc を pi_etc_money にリネームする。

--- lib/orca_api/patient_service/pi_money_etc.rb
+++ lib/orca_api/patient_service/pi_money_etc.rb
@@ -1,6 +1,6 @@
 module OrcaApi
   class PatientService < Service
-    # 患者公費他一部負担額を扱うサービス
+    # 他一部負担額情報を扱うサービス
     #
     # @see http://cms-edit.orca.med.or.jp/_admin/preview_revision/18351#api5
     # @see http://cms-edit.orca.med.or.jp/receipt/tec/api/haori_patientmod.data/api12v035.pdf
@@ -35,6 +35,60 @@ module OrcaApi
         res
       ensure
         unlock(res)
+      end
+
+      # 他一部負担額を更新する
+      #
+      # @params [String] id
+      #   患者ID
+      # @params [String] pi_id
+      #   公費ID
+      # @params [String] number
+      #   公費負担額の連番
+      # @params [String] start_date
+      #   公費負担額の開始日
+      # @params [Hash] args
+      #   他一部負担額情報
+      #   * "Pi_Etc_Money_Mode" (String)
+      #     処理区分。
+      #     "Modify": 更新、"Delete": 削除、変更なしは空白。
+      #     更新は処理単位毎に送信内容で一括更新処理を行います。
+      #     削除は処理単位毎にテーブルを一括削除します。
+      #   * "Pi_Etc_Money_Info" (Array<Hash>)
+      #     他一部負担額情報。
+      #     処理区分が"Delete"の場合は不要です。
+      #     * "Pi_Etc_Money_Date" (String)
+      #       年月日。YYYY-mm-dd形式。必須。
+      #     * "Pi_Etc_Money_InOut" (String)
+      #       入外区分。1: 入院、2: 入院外。必須。
+      #     * "Pi_Etc_Money_Money" (String)
+      #       他一部負担額
+      # @return [OrcaApi::Result]
+      #   日レセからのレスポンス
+      #
+      # @see http://cms-edit.orca.med.or.jp/_admin/preview_revision/18351#api5
+      # @see http://cms-edit.orca.med.or.jp/receipt/tec/api/haori_patientmod.data/api12v035.pdf
+      # @see http://cms-edit.orca.med.or.jp/receipt/tec/api/haori_patientmod.data/api12v035_err.pdf
+      def update(id, pi_id, number, start_date, args)
+        locked_result = res = call_01(id)
+        if !res.ok?
+          return res
+        end
+        res = call_02(pi_id, res)
+        if !res.ok?
+          return res
+        end
+        res = call_04(number, start_date, res)
+        if !res.ok?
+          return res
+        end
+        res = call_05(args, res)
+        if res.ok?
+          locked_result = nil
+        end
+        res
+      ensure
+        unlock(locked_result)
       end
 
       private
@@ -76,6 +130,24 @@ module OrcaApi
             "Pi_Money_Sel_Number" => number.to_s,
             "Pi_Money_Sel_StartDate" => start_date.to_s,
           },
+        }
+        call(req)
+      end
+
+      def call_05(args, res)
+        req = {
+          "Request_Number" => res.response_number,
+          "Karte_Uid" => res.karte_uid,
+          "Orca_Uid" => res.orca_uid,
+          "Patient_Information" => res.patient_information,
+          "PublicInsurance_Information" => {
+            "PublicInsurance_Id" => res.pi_money_information["Sel_PublicInsurance_Id"],
+          },
+          "Pi_Money_Sel_Information" => {
+            "Pi_Money_Sel_Number" => res.pi_etc_money_information["Sel_Pi_Etc_Money_Number"],
+            "Pi_Money_Sel_StartDate" => res.pi_etc_money_information["Sel_Pi_Etc_Money_StartDate"],
+          },
+          "Pi_Etc_Money_Information" => args,
         }
         call(req)
       end
--- spec/orca_api/patient_service/pi_money_etc_spec.rb
+++ spec/orca_api/patient_service/pi_money_etc_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe OrcaApi::PatientService::PiMoneyEtc, orca_api_mock: true do
 
   describe "#get" do
     context "正常系" do
-      it "公費負担額一覧を取得できること" do
+      it "他一部負担額情報の一覧を取得できること" do
         expect_data = [
           {
             path: "/orca12/patientmodv35",
@@ -216,6 +216,426 @@ RSpec.describe OrcaApi::PatientService::PiMoneyEtc, orca_api_mock: true do
         expect_orca_api_call(expect_data, binding)
 
         result = service.get(5, 1, 2, "2012-06-01")
+
+        expect(result.ok?).to be false
+      end
+    end
+  end
+
+  describe "#update" do
+    context "正常系" do
+      it "他一部負担額を更新できること" do
+        args = {
+          "Pi_Etc_Money_Mode": "Modify",
+          "Pi_Etc_Money_Info": [
+            {
+              "Pi_Etc_Money_Date": "2012-05-15",
+              "Pi_Etc_Money_InOut": "2",
+              "Pi_Etc_Money_Money": "0007500"
+            }
+          ]
+        }
+
+        expect_data = [
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "01",
+                "Karte_Uid" => orca_api.karte_uid,
+                "Patient_Information" => {
+                  "Patient_ID" => "5",
+                }
+              }
+            },
+            result: "orca12_patientmodv35_etc_01.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "02",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+                "Patient_Information" => "`prev.patient_information`",
+                "PublicInsurance_Information" => {
+                  "PublicInsurance_Id" => "1",
+                },
+              }
+            },
+            result: "orca12_patientmodv35_etc_02.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "04",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+                "Patient_Information" => "`prev.patient_information`",
+                "PublicInsurance_Information" => {
+                  "PublicInsurance_Id" => '`prev.pi_money_information["Sel_PublicInsurance_Id"]`',
+                },
+                "Pi_Money_Sel_Information" => {
+                  "Pi_Money_Sel_Number" => "2",
+                  "Pi_Money_Sel_StartDate" => "2012-05-01",
+                },
+              }
+            },
+            result: "orca12_patientmodv35_etc_04.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "05",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+                "Patient_Information" => "`prev.patient_information`",
+                "PublicInsurance_Information" => {
+                  "PublicInsurance_Id" => '`prev.pi_money_information["Sel_PublicInsurance_Id"]`',
+                },
+                "Pi_Money_Sel_Information" => {
+                  "Pi_Money_Sel_Number" => '`prev.pi_etc_money_information["Sel_Pi_Etc_Money_Number"]`',
+                  "Pi_Money_Sel_StartDate" => '`prev.pi_etc_money_information["Sel_Pi_Etc_Money_StartDate"]`',
+                },
+                "Pi_Etc_Money_Information" => args,
+              }
+            },
+            result: "orca12_patientmodv35_etc_05_modify.json",
+          },
+        ]
+
+        expect_orca_api_call(expect_data, binding)
+
+        result = service.update(5, 1, 2, "2012-05-01", args)
+
+        expect(result.ok?).to be true
+      end
+
+      it "他一部負担額を削除できること" do
+        args = {
+          "Pi_Etc_Money_Mode": "Delete"
+        }
+
+        expect_data = [
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "01",
+                "Karte_Uid" => orca_api.karte_uid,
+                "Patient_Information" => {
+                  "Patient_ID" => "5",
+                }
+              }
+            },
+            result: "orca12_patientmodv35_etc_01.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "02",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+                "Patient_Information" => "`prev.patient_information`",
+                "PublicInsurance_Information" => {
+                  "PublicInsurance_Id" => "1",
+                },
+              }
+            },
+            result: "orca12_patientmodv35_etc_02.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "04",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+                "Patient_Information" => "`prev.patient_information`",
+                "PublicInsurance_Information" => {
+                  "PublicInsurance_Id" => '`prev.pi_money_information["Sel_PublicInsurance_Id"]`',
+                },
+                "Pi_Money_Sel_Information" => {
+                  "Pi_Money_Sel_Number" => "2",
+                  "Pi_Money_Sel_StartDate" => "2012-05-01",
+                },
+              }
+            },
+            result: "orca12_patientmodv35_etc_04.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "05",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+                "Patient_Information" => "`prev.patient_information`",
+                "PublicInsurance_Information" => {
+                  "PublicInsurance_Id" => '`prev.pi_money_information["Sel_PublicInsurance_Id"]`',
+                },
+                "Pi_Money_Sel_Information" => {
+                  "Pi_Money_Sel_Number" => '`prev.pi_etc_money_information["Sel_Pi_Etc_Money_Number"]`',
+                  "Pi_Money_Sel_StartDate" => '`prev.pi_etc_money_information["Sel_Pi_Etc_Money_StartDate"]`',
+                },
+                "Pi_Etc_Money_Information" => args,
+              }
+            },
+            result: "orca12_patientmodv35_etc_05_delete.json",
+          },
+        ]
+
+        expect_orca_api_call(expect_data, binding)
+
+        result = service.update(5, 1, 2, "2012-05-01", args)
+
+        expect(result.ok?).to be true
+      end
+    end
+
+    context "異常系" do
+      it "患者番号に該当する患者が存在しない場合、ロック解除を行わないこと" do
+        expect_data = [
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "01",
+                "Karte_Uid" => orca_api.karte_uid,
+                "Patient_Information" => {
+                  "Patient_ID" => "999999",
+                }
+              }
+            },
+            result: "orca12_patientmodv35_01_E10.json",
+          },
+        ]
+
+        expect_orca_api_call(expect_data, binding)
+
+        result = service.update(999999, 1, 2, "2012-05-01", {})
+
+        expect(result.ok?).to be false
+      end
+
+      it "公費IDに該当する公費が存在しない場合、ロック解除を行うこと" do
+        expect_data = [
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "01",
+                "Karte_Uid" => orca_api.karte_uid,
+                "Patient_Information" => {
+                  "Patient_ID" => "3",
+                }
+              }
+            },
+            result: "orca12_patientmodv35_01.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "02",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+                "Patient_Information" => "`prev.patient_information`",
+                "PublicInsurance_Information" => {
+                  "PublicInsurance_Id" => "5",
+                },
+              }
+            },
+            result: "orca12_patientmodv35_02_E32.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "99",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+              }
+            },
+            result: "orca12_patientmodv35_99.json",
+          },
+        ]
+
+        expect_orca_api_call(expect_data, binding)
+
+        result = service.update(3, 5, 2, "2012-05-01", {})
+
+        expect(result.ok?).to be false
+      end
+
+      it "対象の公費負担額が存在しない場合、ロック解除を行うこと" do
+        expect_data = [
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "01",
+                "Karte_Uid" => orca_api.karte_uid,
+                "Patient_Information" => {
+                  "Patient_ID" => "5",
+                }
+              }
+            },
+            result: "orca12_patientmodv35_etc_01.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "02",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+                "Patient_Information" => "`prev.patient_information`",
+                "PublicInsurance_Information" => {
+                  "PublicInsurance_Id" => "1",
+                },
+              }
+            },
+            result: "orca12_patientmodv35_etc_02.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "04",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+                "Patient_Information" => "`prev.patient_information`",
+                "PublicInsurance_Information" => {
+                  "PublicInsurance_Id" => '`prev.pi_money_information["Sel_PublicInsurance_Id"]`',
+                },
+                "Pi_Money_Sel_Information" => {
+                  "Pi_Money_Sel_Number" => "2",
+                  "Pi_Money_Sel_StartDate" => "2012-06-01",
+                },
+              }
+            },
+            result: "orca12_patientmodv35_etc_04_E43.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "99",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+              }
+            },
+            result: "orca12_patientmodv35_99.json",
+          },
+        ]
+
+        expect_orca_api_call(expect_data, binding)
+
+        result = service.update(5, 1, 2, "2012-06-01", {})
+
+        expect(result.ok?).to be false
+      end
+
+      it "入外区分エラーが発生した場合、ロック解除を行うこと" do
+        args = {
+          "Pi_Etc_Money_Mode": "Modify",
+          "Pi_Etc_Money_Info": [
+            {
+              "Pi_Etc_Money_Date": "2012-05-15",
+              "Pi_Etc_Money_InOut": "3",
+              "Pi_Etc_Money_Money": "0007500"
+            }
+          ]
+        }
+
+        expect_data = [
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "01",
+                "Karte_Uid" => orca_api.karte_uid,
+                "Patient_Information" => {
+                  "Patient_ID" => "5",
+                }
+              }
+            },
+            result: "orca12_patientmodv35_etc_01.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "02",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+                "Patient_Information" => "`prev.patient_information`",
+                "PublicInsurance_Information" => {
+                  "PublicInsurance_Id" => "1",
+                },
+              }
+            },
+            result: "orca12_patientmodv35_etc_02.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "04",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+                "Patient_Information" => "`prev.patient_information`",
+                "PublicInsurance_Information" => {
+                  "PublicInsurance_Id" => '`prev.pi_money_information["Sel_PublicInsurance_Id"]`',
+                },
+                "Pi_Money_Sel_Information" => {
+                  "Pi_Money_Sel_Number" => "2",
+                  "Pi_Money_Sel_StartDate" => "2012-06-01",
+                },
+              }
+            },
+            result: "orca12_patientmodv35_etc_04.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "05",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+                "Patient_Information" => "`prev.patient_information`",
+                "PublicInsurance_Information" => {
+                  "PublicInsurance_Id" => '`prev.pi_money_information["Sel_PublicInsurance_Id"]`',
+                },
+                "Pi_Money_Sel_Information" => {
+                  "Pi_Money_Sel_Number" => '`prev.pi_etc_money_information["Sel_Pi_Etc_Money_Number"]`',
+                  "Pi_Money_Sel_StartDate" => '`prev.pi_etc_money_information["Sel_Pi_Etc_Money_StartDate"]`',
+                },
+                "Pi_Etc_Money_Information" => args,
+              }
+            },
+            result: "orca12_patientmodv35_etc_05_E52.json",
+          },
+          {
+            path: "/orca12/patientmodv35",
+            body: {
+              "=patientmodv3req5" => {
+                "Request_Number" => "99",
+                "Karte_Uid" => '`prev.karte_uid`',
+                "Orca_Uid" => "`prev.orca_uid`",
+              }
+            },
+            result: "orca12_patientmodv35_99.json",
+          },
+        ]
+
+        expect_orca_api_call(expect_data, binding)
+
+        result = service.update(5, 1, 2, "2012-06-01", args)
 
         expect(result.ok?).to be false
       end
