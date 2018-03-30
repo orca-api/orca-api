c3edf2cf4d5c6a0b674c6f30548cd0b2b4ea2f0e
Kouji Takao <kouji@netlab.jp>
2018-03-30 10:45:21 +0900

    OrcaApi::FindService::FindResultの自動テストの追加

--- spec/orca_api/find_service_spec.rb
+++ spec/orca_api/find_service_spec.rb
@@ -2,6 +2,20 @@ require "spec_helper"
 require_relative "shared_examples"
 
 RSpec.describe OrcaApi::FindService, orca_api_mock: true do
+  describe OrcaApi::FindService::FindResult do
+    describe "doing?" do
+      it "Api_ResultがE1040であればtrueを返すこと" do
+        result = described_class.new(load_orca_api_response("orca13_findv3_02_E1040.json"))
+        expect(result.doing?).to be true
+      end
+
+      it "Api_ResultがE1040でなければfalseを返すこと" do
+        result = described_class.new(load_orca_api_response("orca13_findv3_02.json"))
+        expect(result.doing?).to be false
+      end
+    end
+  end
+
   let(:service) { described_class.new(orca_api) }
 
   describe "#settings" do
