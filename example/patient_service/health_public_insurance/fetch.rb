e16fa7ee91f9301ff7925e6fd5866356da916968
JinOketani <jin.oketani@medley.jp>
2021-01-19 22:06:07 +0900

    保険・公費情報のチェックができるように基準日を追加した

--- example/patient_service/health_public_insurance/fetch.rb
+++ example/patient_service/health_public_insurance/fetch.rb
@@ -2,8 +2,10 @@
 require_relative "../../common"
 
 patient_service = @orca_api.new_patient_service
+id = ARGV.shift
+base_date = ARGV.shift
 
-result = patient_service.fetch_health_public_insurance(ARGV.shift)
+result = patient_service.fetch_health_public_insurance(id, base_date)
 if result.ok?
   pp result.patient_information
   pp result.health_public_insurance
