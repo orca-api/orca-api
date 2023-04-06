3b359250b181290c7f860c1ec4ae6859776bd99c
RUIJIE.HAO <datouhaoruijie@gmail.com>
2023-04-06 20:17:18 +0900

    update

--- lib/orca_api/prescription_print_service.rb
+++ lib/orca_api/prescription_print_service.rb
@@ -4,10 +4,38 @@ require_relative 'service'
 module OrcaApi
   class PrescriptionPrintService < Service
     def get_medical_fee(params)
-      orca_api.call("/api21/medicalmodv31", body: {
-        "medicalv3req1" => params,
-      })
+      body = {
+        "medicalv3req1" => {
+          "Request_Number" => "01",
+          "Karte_Uid" => params["Karte_Uid"],
+          "Patient_ID" => params["Patient_ID"],
+          "Perform_Date" => params["Perform_Date"],
+          "Perform_Time" => params["Perform_Time"],
+          "Patient_Mode" => params["Patient_Mode"],
+          "Diagnosis_Information" => params["Diagnosis_Information"],
+        },
+      }
+
+      orca_api.call("/api21/medicalmodv31", body: body)
     end
+
+    def medical_treatment(params)
+      params = params.merge({
+        "Request_Number" => "02",
+      })
+
+      medical_call(params)
+    end
+
+    def medical_check(params)
+      params = params.merge({
+        "Request_Number" => "03",
+      })
+
+      medical_call(params)
+    end
+
+    private
 
     def medical_call(params)
       orca_api.call("/api21/medicalmodv32", body: {
